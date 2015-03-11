641f1c5fa5a24b590d71962c63bf3d924dd99f88
Flavio Coutinho <fegemo@gmail.com>
2015-03-11 17:10:54 -0300

    Resizes deck parent instead of on each slide
    Sets zoom or scaling (transform: scale) on the deck parent instead of on
    each slide to improve performance on mobile devices.

--- test/spec/bespoke-scaleSpec.js
+++ test/spec/bespoke-scaleSpec.js
@@ -14,8 +14,8 @@ describe("bespoke-scale", function() {
       container = document.createElement('div');
 
       style.innerText =
-        '.bespoke-scale-parent { position: absolute; top: 0; left: 0; right: 0; bottom: 0; }' +
-        '.bespoke-parent { position: absolute; width: ' + width + 'px; height: ' + height + 'px; }' +
+        '.bespoke-parent { position: absolute; top: 0; left: 0; right: 0; bottom: 0; }' +
+        '.bespoke-scale-parent { position: absolute; left: 50%; top: 50%; width: ' + width + 'px; height: ' + height + 'px; margin-left: -' + (width/2) + 'px; margin-top: -' + (height/2) + 'px; }' +
         '.bespoke-slide { position: absolute; left: 50%; top: 50%; width: 100px; height: 100px; margin-left: -50px; margin-top: -50px; }';
 
       for (var i = 0; i < 10; i++) {
@@ -53,39 +53,61 @@ describe("bespoke-scale", function() {
 
     actualSize = function(el, dimension) {
       return el['offset' + (dimension === 'width' ? 'Width' : 'Height')] *
-        (el.style.zoom != null && el.style.zoom !== '' ?
-          el.style.zoom :
+        (el.parentNode.style.zoom != null && el.parentNode.style.zoom !== '' ?
+          el.parentNode.style.zoom :
           el.parentNode.style[transformProperty].match(/scale\(([0-9.]+)\)/)[1]
         );
     };
 
-  [undefined, 'zoom', 'transform'].forEach(function(option) {
+    [undefined, 'zoom', 'transform'].forEach(function (option) {
 
-    describe("option value of '" + option + "'", function() {
+      describe("option value of '" + option + "'", function () {
 
-      describe("loading a 4x3 pressentation", function() {
+        describe("loading a 4x3 pressentation", function () {
 
-        beforeEach(createDeck.bind(null, 1024, 768, option));
+          beforeEach(createDeck.bind(null, 1024, 768, option));
 
-        if (option === 'transform') {
-          it("should wrap each of the slides in a 'bespoke-scale-parent'", function() {
-            deck.slides.forEach(function(slide) {
-              expect(slide.parentNode.className).toBe('bespoke-scale-parent');
+          if (option === 'transform') {
+            it("should wrap each of the slides in a 'bespoke-scale-parent'", function () {
+              deck.slides.forEach(function (slide) {
+                expect(slide.parentNode.className).toBe('bespoke-scale-parent');
+              });
             });
-          });
-        }
+          }
 
-        it("should resize the slide to fit the height of the deck", function() {
-          expect(actualSize(deck.slides[0], 'height')).toBe(deck.parent.offsetHeight);
+          it("should resize the slide to fit the height of the deck", function () {
+            expect(actualSize(deck.slides[0], 'height')).toBe(deck.parent.offsetHeight);
+          });
+
+          describe("resizing the window to a 3x4 ratio", function () {
+
+            beforeEach(function () {
+              resize(deck.parent, 768, 1024, option)
+            });
+
+            it("should resize the slide to fit the width of the deck", function () {
+              expect(actualSize(deck.slides[0], 'width')).toBe(deck.parent.offsetWidth);
+            });
+
+          });
+
         });
 
-        describe("resizing the window to a 3x4 ratio", function() {
+        describe("loading a 16x9 pressentation", function () {
 
-          beforeEach(function() {
-            resize(deck.parent, 768, 1024, option)
+          beforeEach(createDeck.bind(null, 1280, 720, option));
+
+          it("should resize the slide to fit the height of the deck", function () {
+            expect(actualSize(deck.slides[0], 'height')).toBe(deck.parent.offsetHeight);
           });
 
-          it("should resize the slide to fit the width of the deck", function() {
+        });
+
+        describe("loading a 3x4 pressentation", function () {
+
+          beforeEach(createDeck.bind(null, 480, 640, option));
+
+          it("should resize the slide to fit the width of the deck", function () {
             expect(actualSize(deck.slides[0], 'width')).toBe(deck.parent.offsetWidth);
           });
 
@@ -93,28 +115,5 @@ describe("bespoke-scale", function() {
 
       });
 
-      describe("loading a 16x9 pressentation", function() {
-
-        beforeEach(createDeck.bind(null, 1280, 720, option));
-
-        it("should resize the slide to fit the height of the deck", function() {
-          expect(actualSize(deck.slides[0], 'height')).toBe(deck.parent.offsetHeight);
-        });
-
-      });
-
-      describe("loading a 3x4 pressentation", function() {
-
-        beforeEach(createDeck.bind(null, 480, 640, option));
-
-        it("should resize the slide to fit the width of the deck", function() {
-          expect(actualSize(deck.slides[0], 'width')).toBe(deck.parent.offsetWidth);
-        });
-
-      });
-
     });
-
-  });
-
 });
